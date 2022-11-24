8e6ee9a88414a3d57d75afeabf555517e91c4273
OldGodShen <2415479434@qq.com>
2022-11-25 01:31:50 +0800

    Revert "完善判断表单是否正确功能以及提交成功显示"
    This reverts commit 4f060a3b5354a9061af0c8a05e50c57a5b6ce77f.

--- ViewsModels/JinshujuViewModel.cs
+++ ViewsModels/JinshujuViewModel.cs
@@ -46,6 +46,13 @@ namespace OGSToolBox.ViewsModels
             /// </summary>
             public string __typename { get; set; }
         }
+        public class Errors
+        {
+            /// <summary>
+            /// 表单不存在
+            /// </summary>
+            public string message { get; set; }
+        }
         public class Data
         {
             /// <summary>
@@ -60,23 +67,31 @@ namespace OGSToolBox.ViewsModels
             /// 
             /// </summary>
             public Data data { get; set; }
+            public Errors errors { get; set; }
         }
 
 
 
-        public string Id { get; set; } = String.Empty;
+        public string Id { get; set; }
         public string StudentName { get; set; }
         public string ClassId { get; set; }
         public string StudentNum { get; set; }
         public string TeacherName { get; set; }
 
         public Command Submit { get; set; }
+        //public Command Wtf { get; set; }
         public JinshujuViewModel()
         {
             Submit = new Command(DoSubmitAsync);
+            //Wtf = new Command(DoWtf);
         }
 
-        private async void DoSubmitAsync()
+        //private async void DoWtf()
+        //{
+        //    await Shell.Current.GoToAsync("//MainPage");
+        //}
+    //...
+    private async void DoSubmitAsync()
         {
             var client = new HttpClient();
             var request = new HttpRequestMessage
@@ -87,7 +102,7 @@ namespace OGSToolBox.ViewsModels
     {
         { "User-Agent", "OGSToolBox" },
     },
-                Content = new StringContent("{\r\n\t\"operationName\": \"CreatePublishedFormEntry\",\r\n\t\"variables\": {\r\n\t\t\"input\": {\r\n\t\t\t\"formId\": \"" + this.Id + "\",\r\n\t\t\t\"entryAttributes\": {\r\n\t\t\t\t\"field_1\": \"" + this.StudentName + "\",\r\n\t\t\t\t\"field_2\": \"" + this.ClassId + "\",\r\n\t\t\t\t\"field_3\": \"" + this.StudentNum + "\",\r\n\t\t\t\t\"field_4\": \"" + this.TeacherName + "\"\r\n\t\t\t},\r\n\t\t\t\"captchaData\": null,\r\n\t\t\t\"weixinAccessToken\": null,\r\n\t\t\t\"xFieldWeixinOpenid\": null,\r\n\t\t\t\"weixinInfo\": null,\r\n\t\t\t\"prefilledParams\": \"\",\r\n\t\t\t\"embedded\": false,\r\n\t\t\t\"internal\": false,\r\n\t\t\t\"backgroundImage\": false,\r\n\t\t\t\"formMargin\": false,\r\n\t\t\t\"hasPreferential\": false,\r\n\t\t\t\"fillingDuration\": 1,\r\n\t\t\t\"forceSubmit\": false\r\n\t\t}\r\n\t},\r\n\t\"extensions\": {\r\n\t\t\"persistedQuery\": {\r\n\t\t\t\"version\": 1,\r\n\t\t\t\"sha256Hash\": \"0f9106976e7cf5f19e8878877bc8030cddcb7463dd76f4e02bc5c67b5874eeae\"\r\n\t\t}\r\n\t}\r\n}")
+                Content = new StringContent("{\r\n\t\"operationName\": \"CreatePublishedFormEntry\",\r\n\t\"variables\": {\r\n\t\t\"input\": {\r\n\t\t\t\"formId\": \"" + this.Id + "\",\r\n\t\t\t\"entryAttributes\": {\r\n\t\t\t\t\"field_1\": \"" + this.StudentName + "\",\r\n\t\t\t\t\"field_2\": \"" + this.ClassId + "\",\r\n\t\t\t\t\"field_3\": \"" + this.StudentNum + "\",\r\n\t\t\t\t\"field_4\": \""+ this.TeacherName + "\"\r\n\t\t\t},\r\n\t\t\t\"captchaData\": null,\r\n\t\t\t\"weixinAccessToken\": null,\r\n\t\t\t\"xFieldWeixinOpenid\": null,\r\n\t\t\t\"weixinInfo\": null,\r\n\t\t\t\"prefilledParams\": \"\",\r\n\t\t\t\"embedded\": false,\r\n\t\t\t\"internal\": false,\r\n\t\t\t\"backgroundImage\": false,\r\n\t\t\t\"formMargin\": false,\r\n\t\t\t\"hasPreferential\": false,\r\n\t\t\t\"fillingDuration\": 1,\r\n\t\t\t\"forceSubmit\": false\r\n\t\t}\r\n\t},\r\n\t\"extensions\": {\r\n\t\t\"persistedQuery\": {\r\n\t\t\t\"version\": 1,\r\n\t\t\t\"sha256Hash\": \"0f9106976e7cf5f19e8878877bc8030cddcb7463dd76f4e02bc5c67b5874eeae\"\r\n\t\t}\r\n\t}\r\n}")
                 {
                     Headers =
         {
@@ -96,21 +111,12 @@ namespace OGSToolBox.ViewsModels
                 }
             };
             using var response = await client.SendAsync(request);
-            string body = await response.Content.ReadAsStringAsync();
-            if (body == "{\"errors\":[{\"message\":\"表单不存在\"}]}")
-            {
-                await Toast.Make("表单编号错误", CommunityToolkit.Maui.Core.ToastDuration.Short).Show();
-            }
-            else if (this.Id == String.Empty)
-            {
-                await Toast.Make("请输入表单编号", CommunityToolkit.Maui.Core.ToastDuration.Short).Show();
-            }
-            else
-            {
-                Root rt = JsonConvert.DeserializeObject<Root>(body);
-                string rs = rt.data.createPublishedFormEntry.redirectUrl;
-                await Toast.Make("提交成功，成功链接:\n" + rs, CommunityToolkit.Maui.Core.ToastDuration.Short).Show();
-            }
+            var body = await response.Content.ReadAsStringAsync();
+            body = body.Replace("[", string.Empty);
+            body = body.Replace("]", string.Empty);
+            Root rt = JsonConvert.DeserializeObject<Root>(body);
+
+            await Toast.Make("1", CommunityToolkit.Maui.Core.ToastDuration.Short).Show();
         }
     }
 }
