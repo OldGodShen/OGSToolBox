9ee45c5faef0305ac530a85795472035bd647cd2
OldGodShen <2415479434@qq.com>
2022-11-27 23:02:51 +0800

    add codebk

--- ViewsModels/RuoyiLoginModel.cs
+++ ViewsModels/RuoyiLoginModel.cs
@@ -20,6 +20,30 @@ namespace OGSToolBox.ViewsModels
             RuoyiLogin = new Command(DoRuoyiLogin);
         }
 
+        public class Root
+        {
+            /// <summary>
+            /// 操作成功
+            /// </summary>
+            public string msg { get; set; }
+            /// <summary>
+            /// 
+            /// </summary>
+            public string img { get; set; }
+            /// <summary>
+            /// 
+            /// </summary>
+            public int code { get; set; }
+            /// <summary>
+            /// 
+            /// </summary>
+            public string captchaOnOff { get; set; }
+            /// <summary>
+            /// 
+            /// </summary>
+            public string uuid { get; set; }
+        }
+
         private async void DoRuoyiLogin()
         {
             var client = new HttpClient();
@@ -30,7 +54,24 @@ namespace OGSToolBox.ViewsModels
             };
             using var response = await client.SendAsync(request);
             string body = await response.Content.ReadAsStringAsync();
-            Trace.WriteLine(body);
+            Root rt = JsonConvert.DeserializeObject<Root>(body);
+            string img = rt.img;
+            string uuid = rt.uuid;
+            request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://www.jfbym.com/api/YmServer/customApi"),
+                Content = new StringContent("{\r\n\t\"image\": \"" + img + "\",\r\n\t\"type\": \"50100\",\r\n\t\"token\": \"R9o3JnxOCpc4pFIB1g5dOAfuRMBsBbwu5Kf3/AMuCD8\"\r\n}")
+                {
+                    Headers =
+        {
+            ContentType = new MediaTypeHeaderValue("application/json")
+        }
+                }
+            };
+            using var codebk = await client.SendAsync(request);
+            body = await codebk.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
         }
     }
 }
